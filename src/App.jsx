c14bafa82bbc3aac26dc696600a2adf1c5c68d9f
Evangelene <siyinyap@gmail.com>
2024-03-06 19:14:54 +0800

    style: add styling to login and sign up forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import TodoPage from "./pages/TodoPage";
+import "./App.css";
 
 function App() {
   
