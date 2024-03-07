042110b0a77dda87f02318254fbca0f91d0e7d04
Evangelene <siyinyap@gmail.com>
2024-03-08 00:30:11 +0800

    feat: add localStorage for storing todo data

--- src/utils/todoSlice.js
+++ src/utils/todoSlice.js
@@ -1,8 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = JSON.parse(localStorage.getItem("todos")) || [];
+
 const todoSlice = createSlice({
   name: "todos",
-  initialState: [],
+  initialState,
   reducers: {
     addTodo: (state, action) => {
       const newTodo = {
@@ -11,17 +13,20 @@ const todoSlice = createSlice({
         completed: false,
       };
       state.push(newTodo);
+      localStorage.setItem("todos", JSON.stringify(state));
     },
     toggleComplete: (state, action) => {
       const todo = state.find((todo) => todo.id === action.payload);
       if (todo) {
         todo.completed = !todo.completed;
+        localStorage.setItem("todos", JSON.stringify(state));
       }
     },
     deleteTodo: (state, action) => {
       const index = state.findIndex((todo) => todo.id === action.payload);
       if (index !== -1) {
         state.splice(index, 1);
+        localStorage.setItem("todos", JSON.stringify(state));
       }
     },
     editTodo: (state, action) => {
@@ -29,6 +34,7 @@ const todoSlice = createSlice({
       const todo = state.find((todo) => todo.id === id);
       if (todo) {
         todo.text = newText;
+        localStorage.setItem("todos", JSON.stringify(state));
       }
     },
   },
