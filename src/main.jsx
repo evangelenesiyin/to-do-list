9d380fee67b08c3beb66445da265e28f2b305237
Evangelene <siyinyap@gmail.com>
2024-03-07 20:54:37 +0800

    feat: import provider and store to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import App from './App.jsx'
+import store from '../src/utils/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
     <App />
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
