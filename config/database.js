f40116ebf0ecf5d5240789f957783bbcb5d6771f
Evangelene <siyinyap@gmail.com>
2024-06-15 12:06:19 +0800

    feat: setup mongoose

---
+++ config/database.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import debugLib from "debug";
+
+const debug = debugLib("procuratio:config:database");
+
+mongoose.set("debug", true);
+
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    const db = mongoose.connection;
+    debug(`Connected to ${db.name} at ${db.host}:${db.port}`);
+  })
+  .catch((error) => {
+    debug(`Database connection error: ${error}`);
+  });
