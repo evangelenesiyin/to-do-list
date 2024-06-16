8d3c7e9c4d299207863a1350c758d662ad6ce5c7
Evangelene <siyinyap@gmail.com>
2024-06-16 20:00:16 +0800

    feat: add middleware and routes

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
+import express from "express";
 import mongoose from "mongoose";
 import debugLib from "debug";
 import dotenv from "dotenv";
+import path from "path";
+import logger from "morgan";
+import checkToken from "./config/checkToken.js";
+import usersRouter from "./routes/usersRouter.js";
 
+// Load environment variables from .env file
 dotenv.config();
 
 const debug = debugLib("procuratio:config:database");
+const app = express();
+const port = process.env.PORT || 3000;
 
+// Middleware
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.static(path.join(process.cwd(), "dist")));
+
+// Custom middleware to check token
+app.use(checkToken);
+
+// Routes
+app.use("/api/users", usersRouter);
+
+// Database connection
 mongoose.set("debug", true);
-
 mongoose
   .connect(process.env.DATABASE_URL)
   .then(() => {
@@ -17,3 +36,7 @@ mongoose
   .catch((error) => {
     debug(`Database connection error: ${error}`);
   });
+
+app.listen(port, () => {
+  debug(`Express app running on port ${port}`);
+});
